90f86fa0f6acf81702b6887fc322692b5378f6ce
HMBL98 <hugomichelbl@gmail.com>
2020-10-28 14:21:00 -0600

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -2,71 +2,64 @@
 using namespace std;
 
 #define forn(i,a,b) for(int i = a; i < b; i++)
+
 const int maxn = 1e5+10;
-int tree[4*maxn],lazy[4*maxn],A[maxn],N,Q;
+int tree[4*maxn],A[maxn],N,Q;
 
 void build(int node, int start, int end){
 	if(start == end){
-		tree[node] = A[start];
+		// Leaf node will have a single element
+        tree[node] = A[start];
     }else{
 		int mid = (start + end) / 2;
+        // Recurse on the left child
         build(2*node, start, mid);
+        // Recurse on the right child
         build(2*node+1, mid+1, end);
-        tree[node] = tree[2*node] + tree[2*node+1];
+        // Internal node will have the sum of both of its children
+        tree[node] = min(tree[2*node],tree[2*node+1]);
     }
 }
-void update(int node, int start, int end, int l, int r, int val){
-	if(lazy[node] != 0){
-		tree[node] += (end - start + 1) * lazy[node];
-        if(start != end){
-			lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
-        }
-        lazy[node] = 0;
-    }
-    if(start > end || start > r || end < l)return;
-    if(start >= l && end <= r){
-		tree[node] += (end - start + 1) * val;
-        if(start != end){
-			lazy[node*2] += val;
-            lazy[node*2+1] += val;
-        }
-        return;
+void update(int node, int start, int end, int idx, int val){
+	if(start > idx || end < idx)return;
+	if(start == idx && end == idx){
+		// Leaf node
+        A[idx] = val;
+        tree[node] = val;
+		return;
     }
     int mid = (start + end) / 2;
-   	update(node*2, start, mid, l, r, val);
-    update(node*2 + 1, mid + 1, end, l, r, val);
-    tree[node] = tree[node*2] + tree[node*2+1];
+    update(2*node, start, mid, idx, val);
+    update(2*node+1, mid+1, end, idx, val);
+    tree[node] = min(tree[2*node],tree[2*node+1]);
 }
 int query(int node, int start, int end, int l, int r){
-	if(start > end || start > r || end < l)return 0;
-	if(lazy[node] != 0){
-        tree[node] += (end - start + 1) * lazy[node];
-        if(start != end){
-			lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
-        }
-        lazy[node] = 0;
+	if(r < start or end < l){
+		// range represented by a node is completely outside the given range
+        return 1<<20;
     }
-    if(start >= l && end <= r)return tree[node];
+    if(l <= start and end <= r){
+		// range represented by a node is completely inside the given range
+        return tree[node];
+    }
+    // range represented by a node is partially inside and partially outside the given range
     int mid = (start + end) / 2;
-    int p1 = query(node*2, start, mid, l, r);
-    int p2 = query(node*2 + 1, mid + 1, end, l, r);
-    return (p1 + p2);
+    int p1 = query(2*node, start, mid, l, r);
+    int p2 = query(2*node+1, mid+1, end, l, r);
+	return min(p1,p2);
 }
 int main(){
 	cin >> N >> Q;
 	forn(i,1,N+1)cin >> A[i];
 	build(1,1,N);
 	while(Q--){
-		char opc; cin >> opc;
-		int x,y,c;
+		char opc;int x,y; cin >> opc;
 		if(opc == 'q'){
 			cin >> x >> y;
 			cout << query(1,1,N,x,y) << "\n";
 		}else{
-			cin >> x >> y >> c;
-			update(1,1,N,x,y,c);
+			cin >> x >> y;
+			update(1,1,N,x,y);
 		}
 	}
 }
